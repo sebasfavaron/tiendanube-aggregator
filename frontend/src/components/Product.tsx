4d14f70fbafa8720de7a046734bcc148c66645ca
Sebastian Favaron <sebastianfavaron@gmail.com>
2024-03-03 02:04:42 -0300

    Add link to real url on product
    + decode html characters on name and descriptions

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,3 +1,5 @@
+import { decodeHtml } from '../utils';
+
 export type ProductData = {
   name: string;
   description: string;
@@ -13,9 +15,20 @@ type ProductProps = {
   image: string;
   url: string;
 };
-export const Product = ({ name, description, price, image }: ProductProps) => {
+export const Product = ({
+  name,
+  description,
+  price,
+  image,
+  url,
+}: ProductProps) => {
   return (
-    <div className='group relative'>
+    <div
+      className='group relative'
+      onClick={() => {
+        window.open(url, '_blank');
+      }}
+    >
       <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'>
         <img
           src={image}
@@ -28,12 +41,14 @@ export const Product = ({ name, description, price, image }: ProductProps) => {
           <h3 className='text-sm text-gray-700'>
             <a href='#'>
               <span aria-hidden='true' className='absolute inset-0'></span>
-              {name}
+              {decodeHtml(name)}
             </a>
           </h3>
           <p className='text-sm font-medium text-gray-900'>${price}</p>
         </div>
-        <p className='mt-1 text-sm text-left text-gray-500'>{description}</p>
+        <p className='mt-1 text-sm text-left text-gray-500'>
+          {decodeHtml(description)}
+        </p>
       </div>
     </div>
   );
---
+++ frontend/src/utils.ts
@@ -0,0 +1,5 @@
+export const decodeHtml = (html: string) => {
+  const txt = document.createElement('textarea');
+  txt.innerHTML = html;
+  return txt.value;
+};
